f07529b6b3a5bf9362577df62ceacdfa93123d87
Rohit K Viswanath <kvrohit@gmail.com>
2025-01-18 20:57:12 +0530

    Animate the logo + tweak hover on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
       serif: ["charter", "serif"],
       mono: ["monospace"],
     },
-    extend: {colors: {
-      darkVoid: "#161617",
-      white: "#c9c7cd"
-    }},
+    extend: {
+      colors: {
+        darkVoid: "#161617",
+        white: "#c9c7cd",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
